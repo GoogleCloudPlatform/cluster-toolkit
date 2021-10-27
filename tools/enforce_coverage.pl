11675933d9b9a2acf87e18e5c34f7e38401e4c9e
Carlos Boneti <carlosboneti@google.com>
2021-10-27 16:35:58 -0700

    Improved makefile, enforce >= 80% eng converage
    Added enforce_coverage to make sure engine tests have at least 80%
    coverage (or none).
    Added Makefile rule for add-google-license, which can benefit googlers.

---
+++ tools/enforce_coverage.pl
@@ -0,0 +1,25 @@
+#!/bin/perl
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use strict;
+use warnings;
+
+my $min = 80;
+while (<>){
+  print $_;
+  if ( $_ =~ /coverage: (\d+\.\d)%/ ) {
+    die "coverage must be above $min%: $1" if ($1 < $min);
+  }
+}
