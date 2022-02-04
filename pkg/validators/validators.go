f2e14f4a11710c035ce6e7d865c4cb8b82fc1187
Tom Downes <tpdownes@google.com>
2022-02-03 21:29:15 -0600

    Initial support for validator functions
    * TODO: resolve variables so that functions get correct values
    * TODO: test coverage

---
+++ pkg/validators/validators.go
@@ -0,0 +1,40 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validators
+
+import (
+	"fmt"
+	"log"
+)
+
+func TestProjectExists(projectID string) error {
+	// log.Printf("UNIMPLEMENTED: check existence of %s", projectID)
+	return fmt.Errorf("UNIMPLEMENTED: check existence of %s", projectID)
+}
+
+func TestRegionExists(region string) error {
+	log.Printf("UNIMPLEMENTED: check existence of %s", region)
+	return nil
+}
+
+func TestZoneExists(zone string) error {
+	log.Printf("UNIMPLEMENTED: check existence of %s", zone)
+	return nil
+}
+
+func TestZoneInRegion(zone string, region string) error {
+	log.Printf("UNIMPLEMENTED: test if %s in %s", zone, region)
+	return nil
+}
