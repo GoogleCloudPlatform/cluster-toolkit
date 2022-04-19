22a4b64c5e2615b45b6a137048dcc784a7ed9592
Ning Li <ning.li@nag.co.uk>
2022-04-19 10:12:31 +0000

    Give superuser unlimited quota

--- community/front-end/website/ghpcfe/management/commands/custom_setup_command.py
+++ community/front-end/website/ghpcfe/management/commands/custom_setup_command.py
@@ -43,6 +43,9 @@ class Command(BaseCommand):
                 # give the super user all the roles
                 user = User.objects.get(pk=1)
                 user.roles.set(roles, clear=True)
+                # set the super user with unlimited quota
+                user.quota_type = 'u'
+                user.save()
                 # initialise database for Google social login
                 site = Site.objects.get(pk=1)
                 site.name = site_name
