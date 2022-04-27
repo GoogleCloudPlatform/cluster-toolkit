d3ae498bb532202e1ffb1a9beaccfd03a156a4de
Tom Downes <tpdownes@google.com>
2022-04-27 16:59:51 -0500

    Modify sgemm job to use debug partition (to address low C2 quota problems)

--- docs/tutorials/intel-select/job_sgemm.sh
+++ docs/tutorials/intel-select/job_sgemm.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=4
-#SBATCH --ntasks-per-node=30
 #SBATCH --time=01:00:00
 #SBATCH --job-name=clckjob
 #SBATCH --output=job_%j.log
-#SBATCH --partition=compute
+#SBATCH --partition=debug
 
 . /apps/clck/2019.10/bin/clckvars.sh
 
