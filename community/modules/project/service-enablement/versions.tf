fe397f401fa99a179d44d808642f4d4f5c6ba532
Alex Heye <heyealex@google.com>
2022-05-24 20:05:56 +0000

    Update version to 0.7.3

--- community/modules/project/service-enablement/versions.tf
+++ community/modules/project/service-enablement/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/v0.7.2"
+    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/v0.7.3"
   }
 
   required_version = ">= 0.14.0"
