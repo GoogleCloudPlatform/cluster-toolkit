57785b5c5f3c6a8fcf28d5778a9f629b9426e59f
Tom Downes <tpdownes@google.com>
2022-06-10 10:02:13 -0500

    Do not impose maximum version constraint here

--- community/modules/scheduler/htcondor-configure/versions.tf
+++ community/modules/scheduler/htcondor-configure/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.0, < 5.0"
+      version = ">= 3.0"
     }
     random = {
       source  = "hashicorp/random"
