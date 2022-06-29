e2981eb8ff9f61161ba7770fa0af4d147862c64d
Nick Stroud <nickstroud@google.com>
2022-06-28 22:09:10 -0700

    Reduce scopes on Batch service account

--- community/modules/scheduler/cloud-batch-job/variables.tf
+++ community/modules/scheduler/cloud-batch-job/variables.tf
@@ -99,8 +99,15 @@ variable "service_account" {
     scopes = set(string)
   })
   default = {
-    email  = null
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    email = null
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
   }
 }
 
