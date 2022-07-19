bc36d00c1fc4c461c2766480141364b2eff4cbcc
Tom Downes <tpdownes@google.com>
2022-07-19 15:35:20 -0700

    Bump patch release to 1.1.2

--- community/modules/compute/SchedMD-slurm-on-gcp-partition/versions.tf
+++ community/modules/compute/SchedMD-slurm-on-gcp-partition/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-partition/v1.1.1"
+    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-partition/v1.1.2"
   }
 
   required_version = ">= 0.14.0"
--- modules/scripts/startup-script/versions.tf
+++ modules/scripts/startup-script/versions.tf
@@ -30,7 +30,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.1.1"
+    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.1.2"
   }
 
   required_version = ">= 0.14.0"
