36210c03af1e529821286bbded7c8aabf3d13992
Alex Heye <heyealex@google.com>
2022-07-25 19:20:09 +0000

    Update to version 1.2.0

--- community/modules/compute/SchedMD-slurm-on-gcp-partition/versions.tf
+++ community/modules/compute/SchedMD-slurm-on-gcp-partition/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-partition/v1.1.2"
+    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-partition/v1.2.0"
   }
 
   required_version = ">= 0.14.0"
