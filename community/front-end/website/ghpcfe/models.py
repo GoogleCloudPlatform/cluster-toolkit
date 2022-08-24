d0dfb8f372960974ac5ad43a5fadc2d5d01df492
Matthew Street <matthew.street@nag.com>
2022-08-24 09:45:26 +0100

    Increased minimum default disk size of Slurm login node from 20GB to 30GB - more space is needed for the new Slurm v5 image

--- community/front-end/website/ghpcfe/models.py
+++ community/front-end/website/ghpcfe/models.py
@@ -642,9 +642,10 @@ class Cluster(CloudResource):
         default="pd-standard",
     )
     login_node_disk_size = models.PositiveIntegerField(
-        validators=[MinValueValidator(10)],
+        # login node disk must be large enough to hold the SlurmGCP image: >=30GB
+        validators=[MinValueValidator(30)],
         help_text="Boot disk size (in GB)",
-        default=20,
+        default=30,
         blank=True,
     )
     grafana_dashboard_url = models.CharField(
