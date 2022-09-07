872aa06c3298ca6410e225b6cfdafb285f8b4a4f
Nick Stroud <nickstroud@google.com>
2022-09-07 10:59:41 -0700

    Roll version for minor release

--- modules/network/pre-existing-vpc/versions.tf
+++ modules/network/pre-existing-vpc/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:pre-existing-vpc/v1.3.1"
+    module_name = "blueprints/terraform/hpc-toolkit:pre-existing-vpc/v1.4.0"
   }
 
   required_version = ">= 0.14.0"
