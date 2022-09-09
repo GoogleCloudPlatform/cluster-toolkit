435c65e5e66d20ab0b065261c8023e50f68f4515
Nick Stroud <nickstroud@google.com>
2022-09-09 11:51:31 -0700

    Roll patch version

--- modules/monitoring/dashboard/versions.tf
+++ modules/monitoring/dashboard/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:dashboard/v1.4.1"
+    module_name = "blueprints/terraform/hpc-toolkit:dashboard/v1.4.2"
   }
 
   required_version = ">= 0.14.0"
