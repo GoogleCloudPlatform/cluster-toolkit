75181313bacf3eddd211b37759d1ddb780afb434
Alex Heye <heyealex@google.com>
2022-10-04 21:10:34 +0000

    Update version to 1.6.0

--- community/modules/scheduler/SchedMD-slurm-on-gcp-login-node/versions.tf
+++ community/modules/scheduler/SchedMD-slurm-on-gcp-login-node/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-login-node/v1.5.0"
+    module_name = "blueprints/terraform/hpc-toolkit:SchedMD-slurm-on-gcp-login-node/v1.6.0"
   }
 
   required_version = ">= 0.14.0"
