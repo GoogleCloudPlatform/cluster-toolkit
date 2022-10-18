168fba8b04eb0e43a1a2f3d43fec0e657ad5bfe4
ikatsardis <ikatsardis@gmail.com>
2022-10-18 23:27:51 +0100

    Update benchmarks.py
    Adding SuperUserRequiredMixin to benchmarks view

--- community/front-end/website/ghpcfe/views/benchmarks.py
+++ community/front-end/website/ghpcfe/views/benchmarks.py
@@ -19,11 +19,13 @@ from django.views import generic
 from ..forms import BenchmarkForm
 from ..models import Benchmark
 from ..models import Job
+from ..forms import UserUpdateForm, UserAdminUpdateForm
+from ..permissions import SuperUserRequiredMixin
 
 # list views
 
 
-class BenchmarkListView(generic.ListView):
+class BenchmarkListView(SuperUserRequiredMixin, generic.ListView):
     """Custom ListView for Benchmark model"""
 
     model = Benchmark
@@ -54,7 +56,7 @@ class BenchmarkListView(generic.ListView):
 # detail views
 
 
-class BenchmarkDetailView(generic.DetailView):
+class BenchmarkDetailView(SuperUserRequiredMixin, generic.DetailView):
     """Custom DetailView for Benchmark model"""
 
     model = Benchmark
@@ -71,7 +73,7 @@ class BenchmarkDetailView(generic.DetailView):
 # create/update views
 
 
-class BenchmarkCreateView(generic.CreateView):
+class BenchmarkCreateView(SuperUserRequiredMixin, generic.CreateView):
     """Custom CreateView for Benchmark model"""
 
     success_url = reverse_lazy("benchmarks")
