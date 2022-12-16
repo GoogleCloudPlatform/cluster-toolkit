20d8c09a8066c81e8aa8e7b070a96072024f043a
Thiago Sgobe <sgobe@google.com>
2022-12-16 10:49:35 -0300

    change tfconfig to modulereader

--- pkg/sourcereader/embedded.go
+++ pkg/sourcereader/embedded.go
@@ -15,20 +15,78 @@
 package sourcereader
 
 import (
-	"embed"
 	"fmt"
 	"hpc-toolkit/pkg/modulereader"
-
-	"github.com/hashicorp/terraform-config-inspect/tfconfig"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
 )
 
 // ModuleFS contains embedded modules (./modules) for use in building
 // blueprints. The main package creates and injects the modules directory as
 // hpc-toolkit/modules are not accessible at the package level.
-var ModuleFS embed.FS
+var ModuleFS BaseFS
+
+// BaseFS is an extension of the io.fs interface with the functionality needed
+// in CopyDirFromModules. Works with embed.FS and afero.FS
+type BaseFS interface {
+	ReadDir(string) ([]fs.DirEntry, error)
+	ReadFile(string) ([]byte, error)
+}
 
 // EmbeddedSourceReader reads modules from a local directory
 type EmbeddedSourceReader struct{}
+
+// copyDirFromModules copies an FS directory to a local path
+func copyDirFromModules(fs BaseFS, source string, dest string) error {
+	dirEntries, err := fs.ReadDir(source)
+	if err != nil {
+		return err
+	}
+	for _, dirEntry := range dirEntries {
+		entryName := dirEntry.Name()
+		// path package (not path/filepath) should be used for embedded source
+		// as the path separator is a forward slash, even on Windows systems.
+		// https://pkg.go.dev/embed#hdr-Directives
+		entrySource := path.Join(source, entryName)
+		entryDest := filepath.Join(dest, entryName)
+		if dirEntry.IsDir() {
+			if err := os.Mkdir(entryDest, 0755); err != nil {
+				return err
+			}
+			if err = copyDirFromModules(fs, entrySource, entryDest); err != nil {
+				return err
+			}
+		} else {
+			fileBytes, err := fs.ReadFile(entrySource)
+			if err != nil {
+				return err
+			}
+			copyFile, err := os.Create(entryDest)
+			if err != nil {
+				return err
+			}
+			if _, err = copyFile.Write(fileBytes); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// copyFSToTempDir is a temporary workaround until tfconfig.ReadFromFilesystem
+// works against embed.FS.
+// Open Issue: https://github.com/hashicorp/terraform-config-inspect/issues/68
+func copyFSToTempDir(fs BaseFS, modulePath string) (string, error) {
+	tmpDir, err := ioutil.TempDir("", "tfconfig-module-*")
+	if err != nil {
+		return tmpDir, err
+	}
+	err = copyDirFromModules(fs, modulePath, tmpDir)
+	return tmpDir, err
+}
 
 // GetModuleInfo gets modulereader.ModuleInfo for the given kind from the embedded source
 func (r EmbeddedSourceReader) GetModuleInfo(modPath string, kind string) (modulereader.ModuleInfo, error) {
@@ -36,14 +94,8 @@ func (r EmbeddedSourceReader) GetModuleInfo(modPath string, kind string) (module
 		return modulereader.ModuleInfo{}, fmt.Errorf("Source is not valid: %s", modPath)
 	}
 
-	mod, diag := tfconfig.LoadModuleFromFilesystem(tfconfig.WrapFS(ModuleFS), modPath)
-	if diag.HasErrors() {
-		err := fmt.Errorf(diag.Error())
-		return modulereader.ModuleInfo{}, err
-	}
-
 	reader := modulereader.Factory(kind)
-	mi, err := reader.GetInfo(mod.Path)
+	mi, err := reader.GetInfo(modPath)
 	mi.RequiredApis = defaultAPIList(modPath)
 	return mi, err
 }
@@ -186,11 +238,13 @@ func (r EmbeddedSourceReader) GetModule(modPath string, copyPath string) error {
 		return fmt.Errorf("Source is not valid: %s", modPath)
 	}
 
-	mod, diag := tfconfig.LoadModuleFromFilesystem(tfconfig.WrapFS(ModuleFS), modPath)
-	if diag.HasErrors() {
-		err := fmt.Errorf(diag.Error())
+	modDir, err := copyFSToTempDir(ModuleFS, modPath)
+	defer os.RemoveAll(modDir)
+	if err != nil {
+		err = fmt.Errorf("failed to copy embedded module at %s to tmp dir %s: %v",
+			modPath, modDir, err)
 		return err
 	}
 
-	return copyFromPath(mod.Path, copyPath)
+	return copyFromPath(modDir, copyPath)
 }
