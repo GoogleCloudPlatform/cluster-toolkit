15fe783380255fc05bbad9d31b92372f78b1aeeb
Alex Heye <heyealex@google.com>
2023-01-04 00:21:16 +0000

    Automate gpu guest accelerator in vm-instance if not set
    Adds a boilerplate file "gpu_definition.tf" that handled populating the
    guest_accelerator field when not set by the user and when the machine
    type indicates GPUs are available.

---
+++ modules/compute/vm-instance/gpu_definition.tf
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+locals {
+  #
+  # if a machine type is a2-*-?g it will automatically fill in the guest_accelerator structure.
+  #
+  is_a2_vm = length(regexall("a2-[a-z]+-\\d+g", var.machine_type)) > 0
+  accelerator_types = {
+    "highgpu"  = "nvidia-tesla-a100"
+    "megagpu"  = "nvidia-tesla-a100"
+    "ultragpu" = "nvidia-a100-80gb"
+  }
+  generated_guest_accelerator = try([{
+    type  = lookup(local.accelerator_types, regex("a2-([A-Za-z]+)-", var.machine_type)[0], ""),
+    count = one(regex("a2-[A-Za-z]+-(\\d+)", var.machine_type)),
+  }], [])
+  guest_accelerator = local.is_a2_vm ? coalescelist(
+    var.guest_accelerator,
+    local.generated_guest_accelerator,
+  ) : var.guest_accelerator
+}
