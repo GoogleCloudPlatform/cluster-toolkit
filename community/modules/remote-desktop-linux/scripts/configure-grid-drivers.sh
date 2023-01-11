a4a0b8c21f25b6f4417cd408c5520e610a9dbb49
Injae Kwak <ikwak@hotmail.com>
2023-01-11 07:46:31 +0000

    Update configure-grid-drivers.sh

--- community/modules/remote-desktop-linux/scripts/configure-grid-drivers.sh
+++ community/modules/remote-desktop-linux/scripts/configure-grid-drivers.sh
@@ -3,13 +3,14 @@
 # Ensure nvidia grid drivers and other binaries are installed
 PACKAGES="build-essential gdebi-core mesa-utils lightdm"
 
-sudo apt-get install --assume-yes "$PACKAGES"
+DEBIAN_FRONTEND=noninteractive sudo apt update && sudo apt-get install --assume-yes $PACKAGES
 
 # Download and install Nvidia Grid GPU driver
 DRIVER_URL="https://storage.googleapis.com/nvidia-drivers-us-public/GRID/vGPU14.2/NVIDIA-Linux-x86_64-510.85.02-grid.run"
 FILE_PATH="/tmp/grid_driver.run"
 
 wget "$DRIVER_URL" -O "$FILE_PATH"
+sudo chmod 755 "$FILE_PATH"
 
 sudo gdebi "$FILE_PATH" --non-interactive
 
@@ -19,7 +20,7 @@ sudo vglserver_config +glx +s +f -t
 sudo systemctl set-default graphical.target
 
 # Configure lightdm for X
-echo "/usr/sbin/lightdm" >/etc/X11/default-display-manager
+sudo bash -c 'echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager' 
 
 sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
 
