12f9a25112ec0163bb6ed0d25b3b148657464f5f
Nick Stroud <nickstroud@google.com>
2023-01-18 09:05:07 -0800

    Rolling version to 1.11.0

--- modules/compute/vm-instance/versions.tf
+++ modules/compute/vm-instance/versions.tf
@@ -27,10 +27,10 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:vm-instance/v1.10.1"
+    module_name = "blueprints/terraform/hpc-toolkit:vm-instance/v1.11.0"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:vm-instance/v1.10.1"
+    module_name = "blueprints/terraform/hpc-toolkit:vm-instance/v1.11.0"
   }
 
   required_version = ">= 0.14.0"
