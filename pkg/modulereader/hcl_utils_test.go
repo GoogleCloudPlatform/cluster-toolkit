628a03a76dcd4de19f55f02b8b3b0622d78c6ec6
Ivan Orlov <orlov@google.com>
2023-03-03 19:33:40 +0000

    Address comments

--- pkg/modulereader/hcl_utils_test.go
+++ pkg/modulereader/hcl_utils_test.go
@@ -12,12 +12,10 @@ func (s *MySuite) TestNormalizeType(c *C) {
 
 	c.Check(normalizeType("?invalid_type"), Equals, "?invalid_type")
 
-	c.Check(normalizeType("object({b=string,a=number})"), Equals, "object({a=number,b=string})")
-
 	// `any` is special type, check that it works
-	c.Check(normalizeType("object({b=any,a=number})"), Equals, "object({a=number,b=any})")
+	c.Check(normalizeType("object({b=any,a=number})"), Equals, normalizeType("object({a=number,b=any})"))
 
-	c.Check(normalizeType(" object (  {\na=any\n} ) "), Equals, "object({a=any})")
+	c.Check(normalizeType(" object (  {\na=any\n} ) "), Equals, normalizeType("object({a=any})"))
 
-	c.Check(normalizeType(" string # comment"), Equals, "string")
+	c.Check(normalizeType(" string # comment"), Equals, normalizeType("string"))
 }
