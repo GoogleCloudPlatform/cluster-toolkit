1620f04302a422d1254ba5e735569b8d4a4b9225
Ivan Orlov <orlov@google.com>
2023-03-07 22:25:04 +0000

    Bump terraform providers version 4.53.1 -> 4.56.0

--- pkg/modulewriter/tfversions.go
+++ pkg/modulewriter/tfversions.go
@@ -21,11 +21,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.53.1"
+      version = "~> 4.56.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.53.1"
+      version = "~> 4.56.0"
     }
   }
 }
