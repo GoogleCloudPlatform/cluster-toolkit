a12c9d3574fc3ac98faaa78191c05b2950e112ef
Ivan Orlov <camarada.0re1@gmail.com>
2023-04-07 18:09:00 +0000

    version update

--- community/modules/database/slurm-cloudsql-federation/versions.tf
+++ community/modules/database/slurm-cloudsql-federation/versions.tf
@@ -30,10 +30,10 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.15.0"
+    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.16.0"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.15.0"
+    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.16.0"
   }
 
   required_version = ">= 0.13.0"
