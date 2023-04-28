92d4d0d33e1c4ea7addaaf91a63bb25afa2d6a3e
Ivan Orlov <orlov@google.com>
2023-04-28 11:12:49 -0700

    Address `shellcheck -o all wait-for-startup-status.sh` (#1242)

--- community/modules/scripts/wait-for-startup/scripts/wait-for-startup-status.sh
+++ community/modules/scripts/wait-for-startup/scripts/wait-for-startup-status.sh
@@ -13,16 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ -z "${INSTANCE_NAME}" ]; then
+if [[ -z "${INSTANCE_NAME}" ]]; then
 	echo "INSTANCE_NAME is unset"
 	exit 1
 fi
-if [ -z "${ZONE}" ]; then
+if [[ -z "${ZONE}" ]]; then
 	echo "ZONE is unset"
 	exit 1
 fi
-if [ -z "${PROJECT_ID}" ]; then
+if [[ -z "${PROJECT_ID}" ]]; then
 	echo "PROJECT_ID is unset"
+	exit 1
+fi
+if [[ -z "${TIMEOUT}" ]]; then
+	echo "TIMEOUT is unset"
 	exit 1
 fi
 
@@ -30,21 +34,21 @@ fi
 c1grep() { grep "$@" || test $? = 1; }
 
 now=$(date +%s)
-deadline=$(("${now}" + "${TIMEOUT}"))
+deadline=$((now + TIMEOUT))
 error_file=$(mktemp)
 fetch_cmd="gcloud compute instances get-serial-port-output ${INSTANCE_NAME} --port 1 --zone ${ZONE} --project ${PROJECT_ID}"
 
-until [ "${now}" -gt "${deadline}" ]; do
+until [[ now -gt deadline ]]; do
 	FINISH_LINE="startup-script exit status"
 	ser_log=$(
 		set -o pipefail
 		${fetch_cmd} 2>"${error_file}" | c1grep "${FINISH_LINE}"
 	) || {
-		cat "$error_file"
+		cat "${error_file}"
 		exit 1
 	}
 	STATUS=$(sed -r 's/.*([0-9]+)\s*$/\1/' <<<"${ser_log}" | uniq)
-	if [ -n "${STATUS}" ]; then break; fi
+	if [[ -n "${STATUS}" ]]; then break; fi
 	echo "could not detect end of startup script. Sleeping."
 	sleep 5
 	now=$(date +%s)
@@ -52,12 +56,12 @@ done
 
 # This specific text is monitored for in tests, do not change.
 INSPECT_OUTPUT_TEXT="to inspect the startup script output, please run:"
-if [ "${STATUS}" == 0 ]; then
+if [[ "${STATUS}" == 0 ]]; then
 	echo "startup-script finished successfully"
-elif [ "${STATUS}" == 1 ]; then
+elif [[ "${STATUS}" == 1 ]]; then
 	echo "startup-script finished with errors, ${INSPECT_OUTPUT_TEXT}"
 	echo "${fetch_cmd}"
-elif [ "${now}" -ge "${deadline}" ]; then
+elif [[ now -ge deadline ]]; then
 	echo "startup-script timed out after ${TIMEOUT} seconds"
 	echo "${INSPECT_OUTPUT_TEXT}"
 	echo "${fetch_cmd}"
