a3d3841afe1ac6d28ca926f65f8503d1dd7d0377
Ivan Orlov <orlov@google.com>
2023-05-03 12:03:44 -0700

    Update version to 1.17.0 (#1266)

--- community/modules/database/slurm-cloudsql-federation/versions.tf
+++ community/modules/database/slurm-cloudsql-federation/versions.tf
@@ -30,10 +30,10 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.16.0"
+    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.17.0"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.16.0"
+    module_name = "blueprints/terraform/hpc-toolkit:slurm-cloudsql-federation/v1.17.0"
   }
 
   required_version = ">= 0.13.0"
--- community/modules/scheduler/schedmd-slurm-gcp-v5-login/versions.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v5-login/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:schedmd-slurm-gcp-v5-login/v1.16.0"
+    module_name = "blueprints/terraform/hpc-toolkit:schedmd-slurm-gcp-v5-login/v1.17.0"
   }
   required_version = ">= 0.14.0"
 }
