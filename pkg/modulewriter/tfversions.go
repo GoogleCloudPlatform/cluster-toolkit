5ddf6aa4a5096606bb9b42ab4fc2eb1a9f54e0fd
Ivan Orlov <orlov@google.com>
2023-05-07 15:59:31 -0700

    Update the terraform provider versions (#1287)

--- pkg/modulewriter/tfversions.go
+++ pkg/modulewriter/tfversions.go
@@ -21,11 +21,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.61.0"
+      version = "~> 4.63.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.61.0"
+      version = "~> 4.63.1"
     }
   }
 }
