45b047e5d8bc42482cd4c107b4050d65a2ccf525
Nick Stroud <nickstroud@google.com>
2023-05-24 10:51:24 -0700

    Expose csi driver addons in gke-cluster

--- community/modules/scheduler/gke-cluster/versions.tf
+++ community/modules/scheduler/gke-cluster/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.51.0, < 5.0"
+      version = ">= 4.65.0, < 5.0"
     }
   }
   provider_meta "google" {
