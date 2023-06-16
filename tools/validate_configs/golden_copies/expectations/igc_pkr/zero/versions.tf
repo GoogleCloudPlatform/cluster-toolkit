dbfa14b3c2bf5e4f41712d562584a91728ff38e8
Rohit Ramu <roramu@google.com>
2023-06-16 21:31:08 +0000

    Update "golden_copies" and "modulewriter"

--- tools/validate_configs/golden_copies/expectations/igc_pkr/zero/versions.tf
+++ tools/validate_configs/golden_copies/expectations/igc_pkr/zero/versions.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.65.2"
+      version = "~> 4.69.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.65.2"
+      version = "~> 4.69.1"
     }
   }
 }
