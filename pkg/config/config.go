42b85d69d79e6e6566b698092eab211fb931b30f
Nick Stroud <nickstroud@google.com>
2023-07-31 10:33:32 -0700

    Notify users that spack-install has moved to spack-setup

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -76,6 +76,7 @@ var movedModules = map[string]string{
 	"community/modules/scheduler/cloud-batch-job":        "modules/scheduler/batch-job-template",
 	"community/modules/scheduler/cloud-batch-login-node": "modules/scheduler/batch-login-node",
 	"community/modules/scheduler/htcondor-configure":     "community/modules/scheduler/htcondor-setup",
+	"community/modules/scripts/spack-install":            "community/modules/scripts/spack-setup",
 }
 
 // GroupName is the name of a deployment group
