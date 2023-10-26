33bf402eaa82607a027754c6048fb0dce6d7668c
Ivan Orlov <orlov@google.com>
2023-10-26 09:59:26 -0700

    Add Slurm V6 controller, partition & nodeset modules (#1871)
    * `enable_slurm_gcp_plugins`
    * `enable_reconfigure`
    * `enable_cleanup_subscriptions`
    
    * added `login_startup_script` similar to compute and controller ones;
    
    * `enable_debug_logging` & `extra_logging_flags`;
    
    SlurmV6 takes only one IP address in `static_ip`, so we need to limit `static_ips` to at most one element.
    
    * `cloud_logging_filter` & `pubsub_topic` were used for login module in V5 but dependency direction was changed in V6;
    * `controller_instance_id` - doesn't seem to be used anywhere;
    
    * `additional_networks`
    * `nic_type`
    * `access_config`
    * `total_egress_bandwidth_tier`
    *
    
    * `login_network_storage` - use same logic as V5, use only common `network_storage`;
    * `network_ip` - use same logic as V5, use `static_ips` instead;
    * `spot` (+ `termination_action`) - not present in V5, omit in ToolkitV6, though SchedMD supports it. TODO(?); `controller` only, `nodeset` V6 will support it;
    * `network(network_self_link)` - V6 cluster doesn't provide `network` variable. Fallback to just `subnetwork_self_link`. TODO;
    
    Currently it's implicitly limited to many-nodesets to one-partition.
    AI: after aggregation of all nodesets into a list, remove all duplicates.

--- tools/duplicate-diff.py
+++ tools/duplicate-diff.py
@@ -41,15 +41,21 @@ duplicates = [
         "community/modules/compute/schedmd-slurm-gcp-v5-node-group/gpu_definition.tf",
         "community/modules/scheduler/schedmd-slurm-gcp-v5-login/gpu_definition.tf",
         "community/modules/scheduler/schedmd-slurm-gcp-v5-controller/gpu_definition.tf",
+        "community/modules/compute/schedmd-slurm-gcp-v6-nodeset/gpu_definition.tf",
+        "community/modules/scheduler/schedmd-slurm-gcp-v6-controller/gpu_definition.tf",
     ],
     [
         "community/modules/compute/gke-node-pool/threads_per_core_calc.tf",
         "modules/compute/vm-instance/threads_per_core_calc.tf",
     ],
-    [
+    [ # Slurm V5
         "community/modules/compute/schedmd-slurm-gcp-v5-node-group/source_image_logic.tf",
         "community/modules/scheduler/schedmd-slurm-gcp-v5-controller/source_image_logic.tf",
         "community/modules/scheduler/schedmd-slurm-gcp-v5-login/source_image_logic.tf",
+    ],
+    [ # Slurm V6
+        "community/modules/scheduler/schedmd-slurm-gcp-v6-controller/source_image_logic.tf",
+        "community/modules/compute/schedmd-slurm-gcp-v6-nodeset/source_image_logic.tf",
     ],
     [
         "community/modules/scripts/ramble-execute/templates/ramble_execute.yml.tpl",
