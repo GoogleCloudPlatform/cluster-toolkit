3cffbebb35060920b28d2441d6b8fd490f210494
Nick Stroud <nickstroud@google.com>
2023-11-16 16:05:19 -0800

    fix: Use null resource to trigger replacement of VM to avoid dependency on count=0

--- modules/compute/vm-instance/main.tf
+++ modules/compute/vm-instance/main.tf
@@ -135,6 +135,15 @@ resource "google_compute_resource_policy" "placement_policy" {
   }
 }
 
+resource "null_resource" "replace_vm_trigger_from_placement" {
+  triggers = {
+    vm_count                  = try(tostring(var.placement_policy.vm_count), "")
+    availability_domain_count = try(tostring(var.placement_policy.availability_domain_count), "")
+    max_distance              = try(tostring(var.placement_policy.max_distance), "")
+    collocation               = try(var.placement_policy.collocation, "")
+  }
+}
+
 resource "google_compute_instance" "compute_vm" {
   project  = var.project_id
   provider = google-beta
@@ -236,7 +245,7 @@ resource "google_compute_instance" "compute_vm" {
     ]
 
     replace_triggered_by = [
-      google_compute_resource_policy.placement_policy
+      null_resource.replace_vm_trigger_from_placement
     ]
 
     precondition {
