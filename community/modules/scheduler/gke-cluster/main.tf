d2c9f46039347a3f51a4e52175046957798c7cc2
Eimantas Kazakevicius <eimantas.kazakevicius@nag.com>
2024-01-05 10:51:13 +0000

    refactoring #2085 changes per @nick-stroud comments

--- community/modules/scheduler/gke-cluster/main.tf
+++ community/modules/scheduler/gke-cluster/main.tf
@@ -195,7 +195,7 @@ resource "google_container_node_pool" "system_node_pools" {
   }
 
   node_config {
-    labels          = var.kubernetes_labels
+    labels          = var.system_node_pool_kubernetes_labels
     resource_labels = local.labels
     service_account = var.service_account_email
     oauth_scopes    = var.service_account_scopes
@@ -210,15 +210,15 @@ resource "google_container_node_pool" "system_node_pools" {
     #
     # We use COS_CONTAINERD to be compatible with (optional) gVisor.
     # https://cloud.google.com/kubernetes-engine/docs/how-to/sandbox-pods
-    image_type = var.image_type
+    image_type = var.system_node_pool_image_type
 
     shielded_instance_config {
-      enable_secure_boot          = var.enable_secure_boot
+      enable_secure_boot          = var.system_node_pool_enable_secure_boot
       enable_integrity_monitoring = true
     }
 
     gvnic {
-      enabled = var.image_type == "COS_CONTAINERD"
+      enabled = var.system_node_pool_image_type == "COS_CONTAINERD"
     }
 
     # Implied by Workload Identity
