f253ea4c22813749131a86d33f05c38f867828dc
Tom Downes <tpdownes@google.com>
2024-02-15 16:30:34 -0600

    Bump version to v1.28.1

--- community/modules/pubsub/bigquery-sub/versions.tf
+++ community/modules/pubsub/bigquery-sub/versions.tf
@@ -26,10 +26,10 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:bigquery-sub/v1.28.0"
+    module_name = "blueprints/terraform/hpc-toolkit:bigquery-sub/v1.28.1"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:bigquery-sub/v1.28.0"
+    module_name = "blueprints/terraform/hpc-toolkit:bigquery-sub/v1.28.1"
   }
   required_version = ">= 1.0"
 }
