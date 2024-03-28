5e6fbaf541fdb72af56ffadf8c8b7f7a95f41505
Tom Downes <tpdownes@google.com>
2024-03-28 19:37:26 +0000

    Hotfix to version 1.31.1

--- community/modules/compute/htcondor-execute-point/versions.tf
+++ community/modules/compute/htcondor-execute-point/versions.tf
@@ -25,6 +25,6 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:htcondor-execute-point/v1.31.0"
+    module_name = "blueprints/terraform/hpc-toolkit:htcondor-execute-point/v1.31.1"
   }
 }
--- community/modules/scripts/wait-for-startup/versions.tf
+++ community/modules/scripts/wait-for-startup/versions.tf
@@ -26,7 +26,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:wait-for-startup/v1.31.0"
+    module_name = "blueprints/terraform/hpc-toolkit:wait-for-startup/v1.31.1"
   }
 
   required_version = ">= 0.14.0"
