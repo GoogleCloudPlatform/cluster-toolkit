c7054a85ff2da3542901ac2b4fb6cb6f05780be6
Harsh Thakkar <harshthakkar@google.com>
2024-04-18 22:36:41 +0000

    Fix typo in version

--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ HPC deployments on the Google Cloud Platform.`,
 				logging.Fatal("cmd.Help function failed: %s", err)
 			}
 		},
-		Version:     "vv1.32.0",
+		Version:     "v1.32.1",
 		Annotations: annotation,
 	}
 )
--- community/modules/project/service-enablement/versions.tf
+++ community/modules/project/service-enablement/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/vv1.32.0"
+    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/v1.32.1"
   }
 
   required_version = ">= 0.14.0"
