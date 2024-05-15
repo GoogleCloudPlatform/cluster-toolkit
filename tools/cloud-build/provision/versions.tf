39fd72f7aeb1469b6104864f67434c8372c68b37
Tom Downes <tpdownes@google.com>
2024-05-15 09:12:54 -0500

    Adopt google provider 5.x in integration test terraform module

--- tools/cloud-build/provision/versions.tf
+++ tools/cloud-build/provision/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.58.0"
+      version = "~> 5.0"
     }
     external = {
       source  = "hashicorp/external"
