91b787d8f07acdeaef48b0397ac34a2080e9d28c
Harsh Thakkar <harshthakkar@google.com>
2024-05-22 20:46:15 +0000

    Update version number to v1.3.40 as part of release process

--- community/modules/network/private-service-access/versions.tf
+++ community/modules/network/private-service-access/versions.tf
@@ -30,11 +30,11 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:private-service-access/v1.33.0"
+    module_name = "blueprints/terraform/hpc-toolkit:private-service-access/v1.34.0"
   }
 
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:private-service-access/v1.33.0"
+    module_name = "blueprints/terraform/hpc-toolkit:private-service-access/v1.34.0"
   }
 
   required_version = ">= 1.2"
--- community/modules/project/service-enablement/versions.tf
+++ community/modules/project/service-enablement/versions.tf
@@ -22,7 +22,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/v1.33.0"
+    module_name = "blueprints/terraform/hpc-toolkit:service-enablement/v1.34.0"
   }
 
   required_version = ">= 0.14.0"
