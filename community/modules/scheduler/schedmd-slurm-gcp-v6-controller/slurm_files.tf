36a87dfd2dfc39d840eb47fd4f5949c8e11d8101
Tom Downes <tpdownes@google.com>
2024-05-28 16:43:19 +0000

    Adopt Slurm-GCP 6.5.5
    - Always use short hostname even when host returns FQDN

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
@@ -88,7 +88,7 @@ locals {
 }
 
 module "slurm_files" {
-  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_files?ref=6.5.4"
+  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_files?ref=6.5.5"
 
   project_id         = var.project_id
   slurm_cluster_name = local.slurm_cluster_name
