cfa46070442f97ae33e9f770a7b3198bd6fb3ba0
Scott Gordon <scott.gordon@nag.com>
2024-05-30 18:15:00 +0100

    OFE: Fix for c4

--- community/front-end/ofe/website/ghpcfe/cluster_manager/cloud_info.py
+++ community/front-end/ofe/website/ghpcfe/cluster_manager/cloud_info.py
@@ -360,6 +360,7 @@ def _get_gcp_instance_pricing(
             "n2d": "N2D AMD Instance Core",
             "h3": "Compute optimized Core",
             "c3": "Compute optimized Core",
+            "c4": "Compute optimized Core",
             "c2": "Compute optimized Core",
             "c2d": "C2D AMD Instance Core",
             "c3d": "C3D AMD Instance Core",
@@ -413,7 +414,7 @@ def _get_gcp_instance_pricing(
             "h3": "Compute optimized Ram",
             "c2d": "C2D AMD Instance Ram",
             "c3d": "C3D AMD Instance Ram",
-            "c4": "C4 Instance RAM"
+            "c4": "C4 Instance RAM",
             "t2d": "T2D AMD Instance Ram",
             "a2": "A2 Instance Ram",
             "m1": "Memory-optimized Instance Ram",
