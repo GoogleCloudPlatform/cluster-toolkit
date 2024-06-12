858e1debb7d035965d1318fc4ee802998e71f05b
Ivan Orlov <orlov@google.com>
2024-06-12 21:10:57 +0000

    TEST. Slurm scripts `master`

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
@@ -87,7 +87,7 @@ locals {
 }
 
 module "slurm_files" {
-  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_files?ref=6.5.9"
+  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_files?ref=fix_tpu_static"
 
   project_id         = var.project_id
   slurm_cluster_name = local.slurm_cluster_name
