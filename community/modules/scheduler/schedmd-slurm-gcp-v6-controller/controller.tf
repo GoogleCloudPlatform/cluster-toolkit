2d6f7ab851eb3d2889d4e2a11953b888c82ce989
Ivan Orlov <orlov@google.com>
2024-08-06 19:05:16 +0000

    Update SlurmGCP 6.6.0 -> 6.6.1

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/controller.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/controller.tf
@@ -36,7 +36,7 @@ locals {
 
 # INSTANCE TEMPLATE
 module "slurm_controller_template" {
-  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_instance_template?ref=6.6.0"
+  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/slurm_instance_template?ref=6.6.1"
 
   project_id          = var.project_id
   region              = var.region
@@ -92,7 +92,7 @@ locals {
 }
 
 module "slurm_controller_instance" {
-  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/_slurm_instance?ref=6.6.0"
+  source = "github.com/GoogleCloudPlatform/slurm-gcp.git//terraform/slurm_cluster/modules/_slurm_instance?ref=6.6.1"
 
   access_config       = var.enable_controller_public_ips ? [local.access_config] : []
   add_hostname_suffix = false
