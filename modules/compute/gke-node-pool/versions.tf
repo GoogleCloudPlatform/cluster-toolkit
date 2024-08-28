297b0d8cad8091610a18c1cec7bbb5942a0dbf24
Tom Downes <tpdownes@google.com>
2024-08-28 14:36:49 +0000

    Prevent use of google provider 6.0 in gke-node-pool
    The google_container_node_pool resource has a breaking change in TPG 6.0
    that we must first address.

--- modules/compute/gke-node-pool/versions.tf
+++ modules/compute/gke-node-pool/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "> 5.0"
+      version = "~> 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "> 5.0"
+      version = "~> 5.0"
     }
   }
   provider_meta "google" {
