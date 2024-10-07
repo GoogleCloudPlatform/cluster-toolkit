dc3bc6bc3735ef5f719255ee767849092a202e70
Tom Downes <tpdownes@google.com>
2024-10-07 15:27:54 -0500

    Increase version to 1.41.0

--- modules/scheduler/gke-cluster/versions.tf
+++ modules/scheduler/gke-cluster/versions.tf
@@ -30,6 +30,6 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.40.0"
+    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.41.0"
   }
 }
