e6d5c135b88221f3166b3e8db889fb513e3fe6cb
Rohit Ramu <roramu@google.com>
2024-11-14 14:20:11 -0800

    Increase version to 1.42.0

--- modules/scripts/startup-script/versions.tf
+++ modules/scripts/startup-script/versions.tf
@@ -30,7 +30,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.41.0"
+    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.42.0"
   }
 
   required_version = ">= 1.3"
