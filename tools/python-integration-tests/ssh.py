71da978488f435f46169c645caa6ca1cac5b9608
Alyssa <alyssasm@google.com>
2024-12-02 19:36:23 +0000

    Refactor Python Integration Test

---
+++ tools/python-integration-tests/ssh.py
@@ -0,0 +1,74 @@
+# Copyright 2024 "Google LLC"
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import subprocess
+import time
+import paramiko
+
+class SSHManager:
+    # Manages tunnel and SSH connection.
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+       if not cls._instance:
+           cls._instance = super(SSHManager, cls).__new__(cls)
+       return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, 'ssh_client'):
+            self.tunnel = None
+            self.key = None
+            self.ssh_client = None
+
+    def create_tunnel(self, instance_name, port, project_id, zone):
+        iap_tunnel_cmd = [
+            "gcloud", "compute", "start-iap-tunnel", instance_name,
+            "22", "--project", project_id, "--zone", zone,
+            f"--local-host-port=localhost:{port}"
+        ]
+
+        self.tunnel = subprocess.Popen(iap_tunnel_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # Sleep to give the tunnel a few seconds to set up
+        time.sleep(3)
+
+    def get_keypath(self):
+        # Use existing SSH key pair (assuming it's already in ~/.ssh/google_compute_engine)
+        key_path = os.path.expanduser("~/.ssh/google_compute_engine")
+
+        # Add the public key to OS Login
+        public_key_path = key_path + ".pub"
+        subprocess.run(
+            [
+                "gcloud", "compute", "os-login", "ssh-keys", "describe", 
+                "--key-file", public_key_path
+            ], 
+            check=True, capture_output=True
+        )
+        return key_path
+
+    def setup_connection(self, instance_name, port, project_id, zone):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.key = paramiko.RSAKey.from_private_key_file(self.get_keypath())
+        self.create_tunnel(instance_name, port, project_id, zone)
+
+    def close(self):
+        # Closes existing SSH connection and tunnel
+        if self.tunnel:
+            self.tunnel.terminate()
+            self.tunnel = None
+        if self.ssh_client:
+            self.ssh_client.close()
