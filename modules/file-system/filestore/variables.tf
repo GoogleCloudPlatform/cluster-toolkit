fe53043ddb35b13c6609300b9d9f3a94a476e823
Rachael Tamakloe <63983467+RachaelSTamakloe@users.noreply.github.com>
2025-01-13 10:55:41 -0800

    Update modules/file-system/filestore/variables.tf
    Co-authored-by: Tom Downes <tpdownes@users.noreply.github.com>

--- modules/file-system/filestore/variables.tf
+++ modules/file-system/filestore/variables.tf
@@ -92,6 +92,7 @@ variable "filestore_tier" {
       "BASIC_SSD",
       "HIGH_SCALE_SSD",
       "ZONAL",
+      "REGIONAL",
       "ENTERPRISE"
     ], var.filestore_tier)
     error_message = "Allowed values for filestore_tier are 'BASIC_HDD','BASIC_SSD','ZONAL','REGIONAL'.\nhttps://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/filestore_instance#tier\nhttps://cloud.google.com/filestore/docs/reference/rest/v1/Tier."
