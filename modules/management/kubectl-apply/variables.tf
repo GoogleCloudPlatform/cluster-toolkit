8663a77f5c967de7e28be8042c504050d0899ba5
Michal Wysokinski <mwysokinski@google.com>
2025-03-24 17:20:14 +0100

    Add support for Kueue 0.11.1. Consolidate non-TAS and TAS ResourceFlavors to a single ResourceFlavor which supports both.

--- modules/management/kubectl-apply/variables.tf
+++ modules/management/kubectl-apply/variables.tf
@@ -15,7 +15,7 @@
   */
 
 locals {
-  kueue_supported_versions  = ["v0.10.1", "v0.10.0", "v0.9.1", "v0.9.0", "v0.8.1"]
+  kueue_supported_versions  = ["v0.11.1", "v0.10.1", "v0.10.0", "v0.9.1", "v0.9.0", "v0.8.1"]
   jobset_supported_versions = ["v0.7.2", "v0.5.2"]
 }
 
