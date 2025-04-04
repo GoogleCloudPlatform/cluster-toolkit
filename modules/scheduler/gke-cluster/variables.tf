6e591a792d8e701b65022dca0ea6f0f68450b7ee
Mohit Chaurasia <mohitchaurasia@google.com>
2025-04-04 18:36:24 +0000

    Enable support for DNS based endpoint in GKE

--- modules/scheduler/gke-cluster/variables.tf
+++ modules/scheduler/gke-cluster/variables.tf
@@ -510,3 +510,12 @@ variable "enable_gpu_operator" {
   type        = bool
   default     = false
 }
+
+variable "enable_external_dns_endpoint" {
+  description = <<-EOT
+  Allow [DNS-based approach](https://cloud.google.com/kubernetes-engine/docs/concepts/network-isolation#dns-based_endpoint) for accessing the GKE control plane.
+  Refer this [dedicated blog](https://cloud.google.com/blog/products/containers-kubernetes/new-dns-based-endpoint-for-the-gke-control-plane) for more details.
+  EOT
+  type        = bool
+  default     = false
+}
