23584a27df6e4cb1e0eea2e3a295ce006b3c97f6
Scott Gordon <scott.gordon@nag.com>
2025-05-01 15:49:55 +0100

    artifact registry module validation

--- community/modules/container/artifact-registry/variables.tf
+++ community/modules/container/artifact-registry/variables.tf
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Todo: re-add the variable validation in a way that pre-commit/TF is happy with...
-
 variable "project_id" {
   description = "Project ID where the artifact registry and secret are created."
   type        = string
