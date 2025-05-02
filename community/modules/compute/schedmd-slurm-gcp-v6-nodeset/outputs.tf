092a22ccfe8a33b133907ab2eb9ee3e9a9e0e734
abbas1902 <abbasmohamed@google.com>
2025-05-02 00:26:19 +0000

    Relax machine type validation for dws flex

--- community/modules/compute/schedmd-slurm-gcp-v6-nodeset/outputs.tf
+++ community/modules/compute/schedmd-slurm-gcp-v6-nodeset/outputs.tf
@@ -48,8 +48,8 @@ output "nodeset" {
   }
 
   precondition {
-    condition     = can(regex("^(a3-|g2-|a2-|a4-).*", var.machine_type)) || !var.dws_flex.enabled || var.dws_flex.use_bulk_insert
-    error_message = "DWS Flex-Start is only supported on G2, A2, A3 High/Mega/Ultra, A4 machine types."
+    condition     = local.nodeset.gpu.count > 0 || !var.dws_flex.enabled || var.dws_flex.use_bulk_insert
+    error_message = "DWS Flex-Start is only supported for GPU instances"
   }
 
   precondition {
