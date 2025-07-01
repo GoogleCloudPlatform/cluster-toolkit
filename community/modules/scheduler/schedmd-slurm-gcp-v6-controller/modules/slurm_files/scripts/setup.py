64534de7291ffd15d20aec92afb71b2db53c439a
abbas1902 <abbasmohamed@google.com>
2025-07-01 17:17:47 +0000

    Add retry ability for google-cloud-ops-agent.service during setup

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/scripts/setup.py
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/scripts/setup.py
@@ -597,7 +597,14 @@ def setup_cloud_ops() -> None:
     with open("/etc/google-cloud-ops-agent/config.yaml", "w") as f:
         yaml.safe_dump(file, f, sort_keys=False)
 
-    run("systemctl restart google-cloud-ops-agent.service", timeout=90)
+    try:
+        run("systemctl restart google-cloud-ops-agent.service", timeout=30)
+    except subprocess.TimeoutExpired:
+        log.error("google-cloud-ops-agent.service did not restart within 30s.")
+        result=run("journalctl -u google-cloud-ops-agent-opentelemetry-collector.service", timeout=30, shell=True) #Gives more in-depth logs than cloud ops agent
+        if result.stdout:
+            log.error(f"System logs for google-cloud-ops-agent-opentelemetry-collector.service:\n{result.stdout}")
+        raise
 
 
 def main():
