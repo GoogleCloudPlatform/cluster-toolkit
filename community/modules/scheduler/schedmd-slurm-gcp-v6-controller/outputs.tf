jvilarru/hpc-toolkit

Avoid creating a controller in hybrid mode
Add the variable enable_hybrid to the module, propagating it into the
slurm_files submodule, also conditionate the controller creation to this
variable. Adapt the references to this variable by adding a [0] to it.

community/modules/scheduler/schedmd-slurm-gcp-v6-controller/outputs.tf
@@ -19,7 +19,7 @@ output "slurm_cluster_name" {
 
 output "slurm_controller_instance" {
   description = "Compute instance of controller node"
-  value       = google_compute_instance_from_template.controller
+  value       = var.enable_hybrid ? null : google_compute_instance_from_template.controller[0]
 }
 
 output "slurm_login_instances" {
@@ -34,9 +34,9 @@ output "slurm_bucket_path" {
 
 output "instructions" {
   description = "Post deployment instructions."
-  value       = <<-EOT
+  value       = var.enable_hybrid ? "Please use setup.py to prepare the files" : <<-EOT
     To SSH to the controller (may need to add '--tunnel-through-iap'):
-      gcloud compute ssh ${google_compute_instance_from_template.controller.self_link}
+      gcloud compute ssh ${google_compute_instance_from_template.controller[0].self_link}
     
     If you are using cloud ops agent with this deployment,
     you can use the following command to see the logs for the entire cluster or any particular VM host:

GoogleCloudPlatform/cluster-toolkit

community/modules/scheduler/schedmd-slurm-gcp-v6-controller/outputs.tf
@@ -19,7 +19,7 @@ output "slurm_cluster_name" {
 
 output "slurm_controller_instance" {
   description = "Compute instance of controller node"
-  value       = google_compute_instance_from_template.controller
+  value       = var.enable_hybrid ? null : google_compute_instance_from_template.controller[0]
 }
 
 output "slurm_login_instances" {
@@ -45,9 +45,9 @@ output "slurm_bucket_dir" {
 
 output "instructions" {
   description = "Post deployment instructions."
-  value       = <<-EOT
+  value       = var.enable_hybrid ? "Please use install_hybrid.sh to prepare the files" : <<-EOT
     To SSH to the controller (may need to add '--tunnel-through-iap'):
-      gcloud compute ssh ${google_compute_instance_from_template.controller.self_link}
+      gcloud compute ssh ${google_compute_instance_from_template.controller[0].self_link}
     
     If you are using cloud ops agent with this deployment,
     you can use the following command to see the logs for the entire cluster or any particular VM host: