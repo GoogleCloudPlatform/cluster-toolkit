1d8baceb62021106c1332d74f5c6b140d0b2b310
abbas1902 <abbasmohamed@google.com>
2025-07-26 01:17:04 +0000

    Remove check_image validation assertions

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/source_image_logic.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/source_image_logic.tf
@@ -15,18 +15,6 @@
  */
 
 locals {
-  # Currently supported images and projects
-  known_project_families = {
-    schedmd-slurm-public = [
-      "slurm-gcp-6-10-debian-12",
-      "slurm-gcp-6-10-hpc-rocky-linux-8",
-      "slurm-gcp-6-10-ubuntu-2204-lts-nvidia-570",
-      "slurm-gcp-6-10-ubuntu-2404-lts-nvidia-570",
-      "slurm-gcp-6-10-ubuntu-2204-lts-arm64",
-      "slurm-gcp-6-10-ubuntu-2404-lts-arm64"
-    ]
-  }
-
   # This approach to "hacking" the project name allows a chain of Terraform
   # calls to set the instance source_image (boot disk) with a "relative
   # resource name" that passes muster with VPC Service Control rules
@@ -39,44 +27,4 @@ locals {
   )
   source_image_family = try(var.instance_image.family, "")
   source_image        = try(var.instance_image.name, "")
-}
-
-check "image_validation" {
-  data "google_compute_image" "slurm" {
-    family  = try(var.instance_image.family, null)
-    name    = try(var.instance_image.name, null)
-    project = var.instance_image.project
-  }
-
-  assert {
-    condition     = length(regexall("^projects/.+?/global/images/family$", var.instance_image.project)) == 0
-    error_message = "The \"project\" field in var.instance_image no longer supports a long-form ending in \"family\". Specify only the project ID."
-  }
-
-  assert {
-    condition     = var.instance_image_custom || contains(keys(local.known_project_families), data.google_compute_image.slurm.project)
-    error_message = <<-EOD
-      Images in project ${data.google_compute_image.slurm.project} are not published by SchedMD. Images must be created by compatible releases of the Terraform and Packer modules following the guidance at https://goo.gle/hpc-slurm-images. Set var.instance_image_custom to true to silence this error and acknowledge that you are using a compatible image.
-      EOD
-  }
-
-  assert {
-    condition     = !contains(keys(local.known_project_families), data.google_compute_image.slurm.project) || try(contains(local.known_project_families[data.google_compute_image.slurm.project], data.google_compute_image.slurm.family), false)
-    error_message = <<-EOD
-      Image family ${data.google_compute_image.slurm.family} published by SchedMD in project ${data.google_compute_image.slurm.project} is not compatible with this release of the Terraform Slurm modules. Select from known compatible releases:
-      ${join("\n", [for p in try(local.known_project_families[data.google_compute_image.slurm.project], []) : "\t\"${p}\""])}
-      EOD
-  }
-
-  assert {
-    condition     = var.disk_size_gb >= data.google_compute_image.slurm.disk_size_gb
-    error_message = "'disk_size_gb: ${var.disk_size_gb}' is smaller than the image size (${data.google_compute_image.slurm.disk_size_gb}GB), please increase the blueprint disk size"
-  }
-
-  assert {
-    # Condition needs to check the suffix of the license, as prefix contains an API version which can change.
-    # Example license value: https://www.googleapis.com/compute/v1/projects/cloud-hpc-image-public/global/licenses/hpc-vm-image-feature-disable-auto-updates
-    condition     = var.allow_automatic_updates || anytrue([for license in data.google_compute_image.slurm.licenses : endswith(license, "/projects/cloud-hpc-image-public/global/licenses/hpc-vm-image-feature-disable-auto-updates")])
-    error_message = "Disabling automatic updates is not supported with the selected VM image.  More information: https://cloud.google.com/compute/docs/instances/create-hpc-vm#disable_automatic_updates"
-  }
 }
