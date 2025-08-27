49c34235ca481b2bae66b07ce5893f95c72809ae
Neelabh94 <neelgoyal@google.com>
2025-08-27 14:59:24 +0000

    Blueprint and terraform validation changes for A4X Flex-Start support

--- community/modules/compute/schedmd-slurm-gcp-v6-nodeset/outputs.tf
+++ community/modules/compute/schedmd-slurm-gcp-v6-nodeset/outputs.tf
@@ -33,11 +33,6 @@ output "nodeset" {
   }
 
   precondition {
-    condition     = var.accelerator_topology == null || var.enable_placement
-    error_message = "accelerator_topology requires enable_placement to be set to true."
-  }
-
-  precondition {
     condition     = (var.accelerator_topology == null) || try(tonumber(split("x", var.accelerator_topology)[1]) % local.guest_accelerator[0].count == 0, false)
     error_message = "accelerator_topology must be divisible by number of gpus in machine."
   }
@@ -63,6 +58,21 @@ output "nodeset" {
   }
 
   precondition {
+    condition     = var.accelerator_topology == null || (var.enable_placement || var.dws_flex.enabled)
+    error_message = "accelerator_topology requires either enable_placement to be set to true or DWS Flex to be enabled."
+  }
+
+  precondition {
+    condition     = startswith(var.machine_type, "a4x-") && (!var.dws_flex.enabled || var.accelerator_topology == "1x64")
+    error_message = "For A4X, cannot use DWS Flex with accelerator_topology other than 1x64."
+  }
+
+  precondition {
+    condition     = !var.dws_flex.enabled || var.dws_flex.max_run_duration <= 7
+    error_message = "Cannot use DWS Flex with max_run_duration greater than 7."
+  }
+
+  precondition {
     condition     = length(var.zones) == 0 || !var.dws_flex.enabled
     error_message = <<-EOD
       If a DWS Flex is enabled, `var.zones` should be empty.
@@ -76,7 +86,17 @@ output "nodeset" {
 
   precondition {
     condition     = !var.enable_spot_vm || !var.dws_flex.enabled
-    error_message = "Cannot use both Flex-Start and Spot VMs for provisioning."
+    error_message = "Cannot use both DWS Flex and Spot VMs for provisioning."
+  }
+
+  precondition {
+    condition     = startswith(var.machine_type, "a4x-") && (!var.dws_flex.enabled || var.node_count_static == 16)
+    error_message = "For A4X,if DWS Flex is enabled, `node_count_static` should be set to 16."
+  }
+
+  precondition {
+    condition     = startswith(var.machine_type, "a4x-") && (!var.dws_flex.enabled || var.node_count_dynamic_max == 0)
+    error_message = "For A4X, if DWS Flex is enabled, `node_count_dynamic_max` should be set to 0."
   }
 
   precondition {
