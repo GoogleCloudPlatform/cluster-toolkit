37fced52880850d438fb15861c27f01a0d34f5c7
ankitkumar-quad <kumaanki@google.com>
2025-09-18 17:04:25 +0530

    Update versions.tf

--- community/modules/network/private-service-access/versions.tf
+++ community/modules/network/private-service-access/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.40"
+      version = ">= 7.2"
     }
     random = {
       source  = "hashicorp/random"
