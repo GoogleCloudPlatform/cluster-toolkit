97c1d1d62d3841d92a93537f559f663d3fd4e7af
Aslam Mohammad <aslmohammad@google.com>
2025-11-11 08:08:18 +0000

    Increase version to 1.72.0

--- community/modules/file-system/nfs-server/versions.tf
+++ community/modules/file-system/nfs-server/versions.tf
@@ -30,7 +30,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:nfs-server/v1.71.0"
+    module_name = "blueprints/terraform/hpc-toolkit:nfs-server/v1.72.0"
   }
 
   required_version = ">= 0.14.0"
