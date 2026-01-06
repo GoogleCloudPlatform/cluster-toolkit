ef24fb171e1524366e66006c7fe10e2425712dfe
Aslam <aslmohammad@google.com>
2026-01-06 08:42:45 +0000

    Increase version to 1.77.0

--- community/modules/compute/gke-nodeset/versions.tf
+++ community/modules/compute/gke-nodeset/versions.tf
@@ -22,6 +22,6 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:gke-nodeset/v1.76.0"
+    module_name = "blueprints/terraform/hpc-toolkit:gke-nodeset/v1.77.0"
   }
 }
--- modules/scripts/startup-script/versions.tf
+++ modules/scripts/startup-script/versions.tf
@@ -30,7 +30,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.76.0"
+    module_name = "blueprints/terraform/hpc-toolkit:startup-script/v1.77.0"
   }
 
   required_version = ">= 1.5"
