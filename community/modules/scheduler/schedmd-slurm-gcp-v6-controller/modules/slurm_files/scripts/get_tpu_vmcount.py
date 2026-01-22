dd9e1e8d94ab864a4eeb996ee6fabd0bad390f28
Neelabh Goyal <38628079+Neelabh94@users.noreply.github.com>
2026-01-22 12:34:43 +0530

    Updating license agreements to 2026 (#5132)

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/scripts/get_tpu_vmcount.py
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/scripts/get_tpu_vmcount.py
@@ -1,6 +1,6 @@
 #!/slurm/python/venv/bin/python3.13
 
-# Copyright 2024 Google Inc. All rights reserved.
+# Copyright 2026 Google Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
