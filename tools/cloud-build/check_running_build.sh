b8d39a9da1e59c222a891ad961a60b95a53b13fd
Saara Tyagi <saaratyagi@google.com>
2026-02-13 11:26:28 +0000

    fix pre-commit

--- tools/cloud-build/check_running_build.sh
+++ tools/cloud-build/check_running_build.sh
@@ -20,8 +20,8 @@ TRIGGER_BUILD_CONFIG_PATH="$1"
 echo "$TRIGGER_BUILD_CONFIG_PATH"
 
 if [[ "$TRIGGER_BUILD_CONFIG_PATH" == *"onspot"* && "$TRIGGER_BUILD_CONFIG_PATH" == *"PR"* ]]; then
-    echo "Skipping check for onspot PR build: $TRIGGER_BUILD_CONFIG_PATH"
-    exit 0
+	echo "Skipping check for onspot PR build: $TRIGGER_BUILD_CONFIG_PATH"
+	exit 0
 fi
 
 MATCHING_BUILDS=$(gcloud builds list --ongoing --format 'value(id)' --filter="substitutions.TRIGGER_BUILD_CONFIG_PATH=\"$TRIGGER_BUILD_CONFIG_PATH\"")
