2e29d1eb1addea29188a5c5e9bbd8d14f88360c3
Dominik Rabij <dominikrabij@google.com>
2026-03-23 12:20:12 +0100

    feat(gke-cluster): specify min versions for slice controller

--- modules/scheduler/gke-cluster/versions.tf
+++ modules/scheduler/gke-cluster/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 7.13"
+      version = ">= 7.20.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 7.13"
+      version = ">= 7.20.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
