10d329febe67d464d3f79fa983c8d2a46e5273a1
Kadukuntla Poornima <kadupoornima@google.com>
2026-04-06 09:57:44 +0000

    base

--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ var (
 				logging.Fatal("cmd.Help function failed: %s", err)
 			}
 		},
-		Version:     "v1.86.0",
+		Version:     config.GetToolkitVersion(),
 		Annotations: annotation,
 	}
 )
---
+++ pkg/telemetry/collector_util.go
@@ -0,0 +1,38 @@
+// Copyright 2026 "Google LLC"
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package telemetry
+
+import (
+	"hpc-toolkit/pkg/config"
+)
+
+func getBlueprint(args []string) config.Blueprint {
+	if args == nil {
+		return config.Blueprint{}
+	}
+	bp, _, _ := config.NewBlueprint(args[0])
+	return bp
+}
+
+func getEventMetadataKVPairs(sourceMetadata map[string]string) []map[string]string {
+	eventMetadata := make([]map[string]string, 0)
+	for k, v := range sourceMetadata {
+		eventMetadata = append(eventMetadata, map[string]string{
+			"key":   k,
+			"value": v,
+		})
+	}
+	return eventMetadata
+}
