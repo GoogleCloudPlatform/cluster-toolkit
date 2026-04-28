061039094318da7ffb8e3240a43afc8a3241a7d0
arpitagrwal <arpitagrwal@google.com>
2026-04-28 12:18:58 +0000

    adding new iap-policy module.

---
+++ modules/iam/iap-policy/variables.tf
@@ -0,0 +1,27 @@
+# Copyright 2026 "Google LLC"
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project_id" {
+  type        = string
+  description = "Project ID where the backend service resides."
+}
+variable "backend_service_id" {
+  type        = string
+  description = "The ID of the IAP-secured Google Cloud Backend Service (usually obtained from the gke-backend-fetcher module)."
+}
+variable "iap_members" {
+  type        = list(string)
+  description = "List of IAM members to grant the 'roles/iap.httpsResourceAccessor' role (e.g. ['user:example@google.com', 'group:admins@google.com', 'serviceAccount:sa@project.iam.gserviceaccount.com'])."
+  default     = []
+}
