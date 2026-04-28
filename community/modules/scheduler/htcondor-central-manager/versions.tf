249619c9681207cb03fc0e3f6c3ed1a2a5ef84fb
Siddhartha Tamatam <stamatam@google.com>
2026-04-28 09:02:55 +0000

    Increase version to 1.89.0

--- community/modules/scheduler/htcondor-central-manager/versions.tf
+++ community/modules/scheduler/htcondor-central-manager/versions.tf
@@ -26,7 +26,7 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:htcondor-central-manager/v1.88.0"
+    module_name = "blueprints/terraform/hpc-toolkit:htcondor-central-manager/v1.89.0"
   }
 
   required_version = "= 1.12.2"
