240ccb335d438528632c3514e9d45ba016ed7fe2
arpitagrwal <arpitagrwal@google.com>
2026-04-29 09:12:35 +0000

    adding new workload_identity_binding module.

---
+++ modules/project/workload_identity_binding/main.tf
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# This module creates a Workload Identity binding between a Google Service Account (GSA)
+# and a Kubernetes Service Account (KSA).
+data "google_project" "project" {
+  project_id = var.project_id
+}
+resource "google_service_account_iam_member" "main" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.project_id}.svc.id.goog/subject/ns/${var.namespace}/sa/${var.k8s_service_account_name}"
+}
