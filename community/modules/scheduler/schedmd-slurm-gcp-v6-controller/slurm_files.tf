4919b53ed2f1fc087e078889edbf8bb192507a3a
laveen <laveenekka@google.com>
2026-06-26 08:27:32 +0000

    Clean up the files

--- community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
+++ community/modules/scheduler/schedmd-slurm-gcp-v6-controller/slurm_files.tf
@@ -156,8 +156,9 @@ module "slurm_files" {
 
   project_id                    = var.project_id
   slurm_cluster_name            = local.slurm_cluster_name
-  slurm_control_host            = "${local.slurm_cluster_name}-controller.${var.zone}.c.${var.project_id}.internal"
-  slurm_backup_controller_name  = var.enable_backup_controller ? "${local.slurm_backup_controller_name}.${local.backup_zone}.c.${var.project_id}.internal" : null
+  slurm_control_host            = var.enable_backup_controller ? "${local.slurm_cluster_name}-controller-0" : null
+  slurm_control_addr            = var.enable_backup_controller && length(var.static_ips) >= 1 ? var.static_ips[0] : null
+  slurm_backup_controller_name  = var.enable_backup_controller ? local.slurm_backup_controller_name : null
   slurm_backup_controller_ip    = var.enable_backup_controller && length(var.static_ips) >= 2 ? var.static_ips[1] : null
   bucket_dir                    = var.bucket_dir
   bucket_name                   = local.bucket_name
