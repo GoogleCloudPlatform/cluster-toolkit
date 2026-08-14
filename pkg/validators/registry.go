d9e2a30479f9de733a4a5b327df101d9ec66861e
Saumya Sharma <sharmasaumya@google.com>
2026-08-14 11:07:21 +0000

    comment out authorized_cidr in GKE blueprints and validate authorised_cidr

--- pkg/validators/registry.go
+++ pkg/validators/registry.go
@@ -16,6 +16,7 @@ package validators
 
 // Registry maps validation type strings to their corresponding validator implementation.
 var Registry = map[string]RuleValidator{
+	"cidr":              &CIDRValidator{},
 	"regex":             &RegexValidator{},
 	"allowed_enum":      &AllowedEnumValidator{},
 	"range":             &RangeValidator{},
