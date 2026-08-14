b73006d19a1cd3c1ad8c9e9dd5231fafe40e69ac
Sudheer <markapurams@google.com>
2026-08-14 13:55:17 +0000

    feat(testing): cut over daily test suite to Kueue scheduling (#6147)

--- tools/cloud-build/provision/variables.tf
+++ tools/cloud-build/provision/variables.tf
@@ -89,7 +89,10 @@ variable "kueue_migrated_tests" {
     "gke-g4-onspot",
     "gke-h4d-onspot",
     "ml-h4d-onspot-slurm",
-    "h4d-vm"
+    "h4d-vm",
+    "ml-a3-highgpu-onspot-slurm",
+    "ml-a4-highgpu-onspot-slurm",
+    "ml-g4-onspot-slurm"
   ]
 }
 
