0ffd64c172b0f2d68a9beb1f154ad3a4c17dead5
Payal <115600031+PayalJakhar@users.noreply.github.com>
2026-08-20 14:38:25 +0530

    Clarify prompt for missing Terraform state bucket
    Updated prompt message for missing Terraform state bucket to clarify that 'gcluster destroy' will not delete it later.

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -217,7 +217,7 @@ func createGcsBucketsIfMissing(ctx context.Context, bp config.Blueprint) error {
 
 		if errors.Is(err, storage.ErrBucketNotExist) {
 			if !flagAutoApprove {
-				prompt := fmt.Sprintf("The required Terraform state bucket '%s' is missing. We can create it now, but for data safety, gcluster destroy will not delete it later—you must delete it manually when done.\nAlternatively, you can create it manually via `gcloud storage buckets create gs://%s` and then re-run this command.\nCreate it automatically? [y/N]: ", bucketName, bucketName)
+				prompt := fmt.Sprintf("The required Terraform state bucket '%s' is missing. We can create it now, but for data safety, 'gcluster destroy' will not delete it later. You must delete it manually when done.\nAlternatively, you can create it manually via `gcloud storage buckets create gs://%s` and then re-run this command.\nCreate it automatically? [y/N]: ", bucketName, bucketName)
 				if !confirmActionFunc(prompt) {
 					return fmt.Errorf("user aborted")
 				}
