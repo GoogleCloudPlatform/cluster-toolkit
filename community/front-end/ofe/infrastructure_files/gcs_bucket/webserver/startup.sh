a0bb7a7a3819f42bfad3b42881f0b23659fee29c
Aslam <aslmohammad@google.com>
2026-08-28 05:09:31 +0000

    Replace obsoleted google-cloud-sdk with google-cloud-cli

--- community/front-end/ofe/infrastructure_files/gcs_bucket/webserver/startup.sh
+++ community/front-end/ofe/infrastructure_files/gcs_bucket/webserver/startup.sh
@@ -51,7 +51,7 @@ dnf update -y --security
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 
 dnf install -y terraform
-dnf install --best -y google-cloud-sdk nano make gcc python3.12-devel unzip git \
+dnf install --best -y google-cloud-cli nano make gcc python3.12-devel unzip git \
 	rsync wget nginx bind-utils policycoreutils-python-utils \
 	packer supervisor python3-certbot-nginx jq
 curl --silent --show-error --location https://github.com/mikefarah/yq/releases/download/v4.13.4/yq_linux_amd64 --output /usr/local/bin/yq
