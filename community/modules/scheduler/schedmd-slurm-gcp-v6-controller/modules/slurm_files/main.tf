jvilarru/hpc-toolkit

Generate the necessary files for hybrid setup

community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/main.tf
@@ -182,10 +182,11 @@ locals {
 
   slurm_gcp_devel_zip        = "slurm-gcp-devel.zip"
   slurm_gcp_devel_zip_bucket = format("%s/%s", local.bucket_dir, local.slurm_gcp_devel_zip)
+  devel_zip_directory        = var.enable_hybrid ? local.output_dir : local.build_dir
 }
 
 data "archive_file" "slurm_gcp_devel_zip" {
-  output_path = "${local.build_dir}/${local.slurm_gcp_devel_zip}"
+  output_path = "${local.devel_zip_directory}/${local.slurm_gcp_devel_zip}"
   type        = "zip"
   source_dir  = local.scripts_dir
 

GoogleCloudPlatform/cluster-toolkit

community/modules/scheduler/schedmd-slurm-gcp-v6-controller/modules/slurm_files/main.tf
@@ -195,6 +195,7 @@ locals {
   slurm_gcp_devel_compute_zip        = "slurm-gcp-devel.zip"
   slurm_gcp_devel_zip_bucket         = format("%s/%s", local.bucket_dir, local.slurm_gcp_devel_controller_zip)
   slurm_gcp_devel_compute_zip_bucket = format("%s/%s", local.bucket_dir, local.slurm_gcp_devel_compute_zip)
+  devel_zip_directory                = var.enable_hybrid ? local.output_dir : local.build_dir
 
   controller_files = [
     "tools/gpu-test",
@@ -264,7 +265,7 @@ data "archive_file" "slurm_gcp_devel_controller_zip" {
 }
 
 data "archive_file" "slurm_gcp_devel_compute_zip" {
-  output_path = "${local.build_dir}/${local.slurm_gcp_devel_compute_zip}"
+  output_path = "${local.devel_zip_directory}/${local.slurm_gcp_devel_compute_zip}"
   type        = "zip"
 
   dynamic "source" {