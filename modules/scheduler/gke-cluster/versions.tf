0fdd7641c7ab9e2e6d17d0472cb7a382fa4e5b91
Siddhartha <stamatam@google.com>
2026-09-18 07:32:30 +0000

    Update Toolkit release to v1.104.0 (#6347)

--- modules/scheduler/gke-cluster/versions.tf
+++ modules/scheduler/gke-cluster/versions.tf
@@ -35,10 +35,10 @@ terraform {
 
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.103.0"
+    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.104.0"
   }
 
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.103.0"
+    module_name = "blueprints/terraform/hpc-toolkit:gke-cluster/v1.104.0"
   }
 }
